8a7d499a74101b3abde73e19d0f28c86ee3a32b5
wuqiang <happyboy_tu@163.com>
2019-12-23 22:07:41 +0800

    代理邀请调整分享

--- src/views/HomeAgent.vue
+++ src/views/HomeAgent.vue
@@ -36,7 +36,7 @@
       <ul>
         <li>
           <img class="left-logo" src="@/assets/images/icon_invite.svg" alt="">
-          <div class="right-box" @click="$router.push('/share')">
+          <div class="right-box" @click="$router.push('/invite-agents')">
             <span>代理邀请</span>
             <img class="micon-right" src="@/assets/images/icon_right.svg" alt="">
           </div>
